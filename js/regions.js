009a41b4bac66bb301695ef6731555e4b230a58d
Nicholas Gunner <support@getbeyondwords.com>
2018-05-27 12:41:26 -0600

    added development regions

---
+++ js/regions.js
@@ -0,0 +1,127 @@
+var regionsData = {
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "html": '<div class="o-tile"><div class="o-tile-img Point_of_interest"><img src="https://westfieldny.com/sites/default/files/styles/tile_images_200_x_200_/public/2017-12/1N1A1793_0_0.JPG?itok=MbxzuAa5" width="200" height="200" alt="Memorial Day Breakfast" class="img-fluid"></div><div class="o-tile-content"><div class="o-tile-content-info">Development Region</div><div class="o-tile-content-title">Barcelona</div></div></div>',
+        "color": "#A8E6CF"
+      },
+      "geometry": {
+        "type": "Polygon",
+        "coordinates": [
+          [
+            [
+              -79.61255550384521,
+              42.332153998913704
+            ],
+            [
+              -79.6089506149292,
+              42.330186998977
+            ],
+            [
+              -79.58324432373047,
+              42.34249559941566
+            ],
+            [
+              -79.59071159362792,
+              42.348871014730854
+            ],
+            [
+              -79.6110963821411,
+              42.34002138352224
+            ],
+            [
+              -79.61255550384521,
+              42.332153998913704
+            ]
+          ]
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "html": '<div class="o-tile"><div class="o-tile-img Point_of_interest"><img src="https://westfieldny.com/sites/default/files/styles/tile_images_200_x_200_/public/econ-images/2018-05/water_plant.jpg?itok=AlMRkujG" width="200" height="200" alt="Westfield water plant" class="img-fluid"></div><div class="o-tile-content"><div class="o-tile-content-info">Development Region</div><div class="o-tile-content-title">Throughway Corridor</div></div></div>',
+        "color": "#FFD3B6"
+      },
+      "geometry": {
+        "type": "Polygon",
+        "coordinates": [
+          [
+            [
+              -79.59925174713133,
+              42.3346919724542
+            ],
+            [
+              -79.58981037139893,
+              42.3222231932214
+            ],
+            [
+              -79.56805229187012,
+              42.33304230130273
+            ],
+            [
+              -79.56337451934814,
+              42.340655807135605
+            ],
+            [
+              -79.57135677337646,
+              42.34658735819043
+            ],
+            [
+              -79.58315849304198,
+              42.34243215919873
+            ],
+            [
+              -79.59925174713133,
+              42.3346919724542
+            ]
+          ]
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "html": '<div class="o-tile"><div class="o-tile-img Point_of_interest"><img src="https://westfieldny.com/sites/default/files/styles/tile_images_200_x_200_/public/2018-01/1N1A1807.JPG?itok=5FyVtFkz" width="200" height="200" alt="Downtown Westfield" class="img-fluid"></div><div class="o-tile-content"><div class="o-tile-content-info">Development Region</div><div class="o-tile-content-title">Downtown Core</div></div></div>',
+        "color": "#FF8B94"
+      },
+      "geometry": {
+        "type": "Polygon",
+        "coordinates": [
+          [
+            [
+              -79.58972454071044,
+              42.322159732553295
+            ],
+            [
+              -79.59135532379149,
+              42.31273511283905
+            ],
+            [
+              -79.58573341369629,
+              42.30695905916094
+            ],
+            [
+              -79.56449031829834,
+              42.308926785323266
+            ],
+            [
+              -79.56165790557861,
+              42.32596725934464
+            ],
+            [
+              -79.56796646118164,
+              42.33294712664685
+            ],
+            [
+              -79.58972454071044,
+              42.322159732553295
+            ]
+          ]
+        ]
+      }
+    }
+  ]
+};
--- js/scripts.js
+++ js/scripts.js
@@ -9,10 +9,70 @@ var map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/westfieldny/cjcxob2e71c352smnmnuscxbn',
     center: [-79.585621, 42.329138],
-    zoom: 13,
+    zoom: 13.5,
     minZoom: 10,
     maxBounds: bounds
 });
 
 // Add zoom and rotation controls to the map.
 map.addControl(new mapboxgl.NavigationControl());
+
+map.on('load', function () {
+
+    map.addLayer({
+        'id': 'Development Regions',
+        'type': 'fill',
+        'source': {
+            'type': 'geojson',
+            'data': regionsData
+        },
+        'layout': {},
+        'paint': {
+            'fill-color': ['get', 'color'],
+            'fill-opacity': 0.4
+        }
+    });
+    map.on('click', 'Development Regions', function (e) {
+      new mapboxgl.Popup()
+          .setLngLat(e.lngLat)
+          .setHTML(e.features[0].properties.html)
+          .addTo(map);
+    });
+    map.on('mouseenter', 'Development Regions', function () {
+        map.getCanvas().style.cursor = 'pointer';
+    });
+    map.on('mouseleave', 'Development Regions', function () {
+        map.getCanvas().style.cursor = '';
+    });
+
+});
+
+var toggleableLayerIds = [ 'Development Regions' ];
+
+for (var i = 0; i < toggleableLayerIds.length; i++) {
+    var id = toggleableLayerIds[i];
+
+    var link = document.createElement('a');
+    link.href = '#';
+    link.className = 'active';
+    link.textContent = id;
+
+    link.onclick = function (e) {
+        var clickedLayer = this.textContent;
+        e.preventDefault();
+        e.stopPropagation();
+
+        var visibility = map.getLayoutProperty(clickedLayer, 'visibility');
+
+        if (visibility === 'visible') {
+            map.setLayoutProperty(clickedLayer, 'visibility', 'none');
+            this.className = '';
+        } else {
+            this.className = 'active';
+            map.setLayoutProperty(clickedLayer, 'visibility', 'visible');
+        }
+    };
+
+    var layers = document.getElementById('menu');
+    layers.appendChild(link);
+}
