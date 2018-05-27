3ed79be6a4f8c3ea4a190eb46cb66effe6aa803b
Nicholas Gunner <support@getbeyondwords.com>
2018-05-27 14:40:08 -0600

    added geojson feed

--- js/scripts.js
+++ js/scripts.js
@@ -19,50 +19,75 @@ map.addControl(new mapboxgl.NavigationControl());
 
 map.on('load', function () {
 
-    map.addLayer({
-        'id': 'Development Regions',
-        'type': 'fill',
-        'source': {
-            'type': 'geojson',
-            'data': regionsData
-        },
-        'layout': {},
-        'paint': {
-            'fill-color': ['get', 'color'],
-            'fill-opacity': 0.4
-        }
-    });
-    map.on('click', 'Development Regions', function (e) {
-      new mapboxgl.Popup()
-          .setLngLat(e.lngLat)
-          .setHTML(e.features[0].properties.html)
-          .addTo(map);
-    });
-    map.on('mouseenter', 'Development Regions', function () {
-        map.getCanvas().style.cursor = 'pointer';
-    });
-    map.on('mouseleave', 'Development Regions', function () {
-        map.getCanvas().style.cursor = '';
-    });
+  // REGIONS
+  map.addLayer({
+      'id': 'Development Regions',
+      'type': 'fill',
+      'source': {
+          'type': 'geojson',
+          'data': regionsData
+      },
+      'layout': {},
+      'paint': {
+          'fill-color': ['get', 'color'],
+          'fill-opacity': 0.4
+      }
+  });
+  map.on('click', 'Development Regions', function (e) {
+    new mapboxgl.Popup()
+        .setLngLat(e.lngLat)
+        .setHTML(e.features[0].properties.html)
+        .addTo(map);
+  });
+  map.on('mouseenter', 'Development Regions', function () {
+      map.getCanvas().style.cursor = 'pointer';
+  });
+  map.on('mouseleave', 'Development Regions', function () {
+      map.getCanvas().style.cursor = '';
+  });
 
-    map.addSource("projects", {
-      "type": "geojson",
-      "data": "https://westfieldny.com/api/geojson/projects"
-    });
-    map.addLayer({
-      "id": "projects",
-      "type": "circle",
-      "source": "projects",
-      "paint": {
-        "circle-radius": 10,
-        "circle-color": "#ff442b",
-        "circle-opacity": 1
-       }
+  // PROJECTS
+  map.addSource("projects", {
+    "type": "geojson",
+    "data": "https://westfieldny.com/api/geojson/projects"
+  });
+  map.addLayer({
+    "id": "Projects",
+    "type": "circle",
+    "source": "projects",
+    "paint": {
+      "circle-radius": 10,
+      "circle-color": [
+        'match',
+        ['get', 'status'],
+        'Proposed', '#69D2E7',
+        'Underway', '#E0E4CC',
+        'Complete', '#FA6900',
+        /* other */ '#ccc'
+      ],
+      "circle-opacity": 1
+     }
+   });
+   map.on('click', 'Projects', function (e) {
+     var projectUrl = 'https://westfieldny.com' + e.features[0].properties.path;
+     var projectImg = e.features[0].properties.image;
+     var projectInfo = e.features[0].properties.status + ' ' + e.features[0].properties.type;
+     var projectLabel = e.features[0].properties.name;
+     new mapboxgl.Popup()
+         .setLngLat(e.lngLat)
+         .setHTML('<div class="o-tile"><div class="o-tile-img Point_of_interest"><a href="' + projectUrl + '"><img src="https://westfieldny.com' + projectImg + '" width="200" height="200" alt="' + e.features[0].properties.name + '" class="img-fluid"></a></div><div class="o-tile-content"><div class="o-tile-content-info">' + projectInfo + '</div><div class="o-tile-content-title"><a href="' + projectUrl + '">' + projectLabel + '<p><span class="badge badge-secondary">Learn more</span></p></a></div></div></div>')
+         .addTo(map);
+   });
+   map.on('mouseenter', 'Projects', function () {
+       map.getCanvas().style.cursor = 'pointer';
+   });
+   map.on('mouseleave', 'Projects', function () {
+       map.getCanvas().style.cursor = '';
    });
 
 });
 
-var toggleableLayerIds = [ 'Development Regions' ];
+var toggleableLayerIds = [ 'Development Regions', 'Projects' ];
 
 for (var i = 0; i < toggleableLayerIds.length; i++) {
     var id = toggleableLayerIds[i];
