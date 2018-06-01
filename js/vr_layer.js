28fc835ac1132a33ebcef872e13582fef82f7219
Nicholas Gunner <support@getbeyondwords.com>
2018-06-01 09:39:01 -0600

    added hightway vr

--- js/vr_layer.js
+++ js/vr_layer.js
@@ -10,6 +10,16 @@ var vrLayer = {
             "type": "Point",
             "coordinates": [-79.595153, 42.344188]
         }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+            "content": "<iframe class='vr' allowfullscreen='true' scrolling='no' src='vr/highway_corridor.html' style='border:0;'></iframe><p style='margin:5px 20px 10px;' class='lead'>Bird's Eye: Highway Corridor</p>"
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [-79.581335, 42.329108]
+        }
       }
     ]
   };
