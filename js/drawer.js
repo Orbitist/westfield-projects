3c712fedaa5f63ac77cf79e31f161fdef7cf5128
Nicholas Gunner <ngunner@orbitist.com>
2018-06-12 21:30:23 -0600

    set slick to infinate

--- js/drawer.js
+++ js/drawer.js
@@ -27,14 +27,13 @@ $(document).ready(function(){
     speed: 300,
     slidesToShow: 4,
     slidesToScroll: 3,
+    infinite: true,
     responsive: [
       {
         breakpoint: 1024,
         settings: {
           slidesToShow: 5,
-          slidesToScroll: 4,
-          infinite: true,
-          dots: true
+          slidesToScroll: 4
         }
       },
       {
@@ -51,9 +50,6 @@ $(document).ready(function(){
           slidesToScroll: 2
         }
       }
-      // You can unslick at a given breakpoint now by adding:
-      // settings: "unslick"
-      // instead of a settings object
     ]
   });
 });
