53b07b1434e1e69f5cb06da92b864dfcee2d4a98
Nicholas Gunner <ngunner@orbitist.com>
2018-06-14 01:14:05 -0600

    fixed drawer

--- js/drawer.js
+++ js/drawer.js
@@ -32,22 +32,22 @@ $(document).ready(function(){
       {
         breakpoint: 1024,
         settings: {
-          slidesToShow: 5,
-          slidesToScroll: 4
+          slidesToShow: 3,
+          slidesToScroll: 3
         }
       },
       {
         breakpoint: 600,
         settings: {
-          slidesToShow: 4,
-          slidesToScroll: 3
+          slidesToShow: 2,
+          slidesToScroll: 2
         }
       },
       {
         breakpoint: 480,
         settings: {
-          slidesToShow: 3,
-          slidesToScroll: 2
+          slidesToShow: 1,
+          slidesToScroll: 1
         }
       }
     ]
