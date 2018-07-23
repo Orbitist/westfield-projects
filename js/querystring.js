f8473b840640d35ee1d9c77f0bd4a66113cd06d3
Nicholas Gunner <ngunner@orbitist.com>
2018-07-22 21:45:47 -0600

    added basic query string functionality for project type

---
+++ js/querystring.js
@@ -0,0 +1,26 @@
+function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, '\\$&');
+    var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, ' '));
+}
+
+var queryType = getParameterByName('type');
+
+$('#projectType').val(queryType);
+
+map.on('load', function(e) {
+  formProjectType = $("#projectType").find("option:selected").val();
+  typeFilter = ["==", 'type', formProjectType];
+  buildProjectsFilter();
+  if (formProjectStatus == 'Any Project Status' && formProjectType == 'Any Project Type'){
+    map.setFilter('projects');
+    runStats();
+  } else {
+    map.setFilter('projects', projectsFilterParams);
+    runStats();
+  }
+});
